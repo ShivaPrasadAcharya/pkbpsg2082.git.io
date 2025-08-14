3f50647c4cee604e703694bc22dd2016aa500221
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-08-14 08:55:51 +0545

    Update data6IMAGESS.js

--- data6IMAGESS.js
+++ data6IMAGESS.js
@@ -1,10 +1,11 @@
 window.data6IMAGESS = `
 ID,Title,Description,Image,Password
 001,marksheet2,"PSMII-2082, IInd schedule test",marksheet2.jpg,marksheet2
-002,Gurupurnima-2081,BPSG-2nd Batch,groupphoto2.jpg,null
+
 `;
 window.data6IMAGESSInfo = {
     name: "LockedImages",
     emoji: "🔒",
     description: "Images with optional password protection for viewing and copying/downloading."
 };
+
