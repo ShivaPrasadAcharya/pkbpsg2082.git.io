806a67d71742786240faa5d7041279dc8b2d17b2
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-08-16 15:34:36 +0545

    Update data6IMAGESS.js

--- data6IMAGESS.js
+++ data6IMAGESS.js
@@ -1,6 +1,7 @@
 window.data6IMAGESS = `
 ID,Title,Description,Image,Password
-001,marksheet2,"PSMII-2082, IInd schedule test",marksheet2.jpg,marksheet2
+001,marksheet2082,"PSMII-2082, IInd schedule test",marksheet2082.jpg,marksheet2082
+002,marksheet2081,"PSMII-2081, Ist schedule test",marksheet2081.jpg,marksheet2081
 
 `;
 window.data6IMAGESSInfo = {
@@ -8,4 +9,5 @@ window.data6IMAGESSInfo = {
     emoji: "🔒",
     description: "Images with optional password protection for viewing and copying/downloading."
 };
+
 
